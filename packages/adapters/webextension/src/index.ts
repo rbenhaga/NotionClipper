e52887810bd0177dc15def8823c658dc83775e1e
Rayane Ben Haga <rayanebenhaga@gmail.com>
2025-10-05 16:57:06 +0200

    wip: webextension adapter issues - need debugging

---
+++ packages/adapters/webextension/src/index.ts
@@ -0,0 +1,4 @@
+export { WebExtensionStorageAdapter } from './storage.adapter';
+export { WebExtensionClipboardAdapter } from './clipboard.adapter';
+export { WebExtensionConfigAdapter } from './config.adapter';
+export { WebExtensionNotionAPIAdapter } from './notion-api.adapter';
